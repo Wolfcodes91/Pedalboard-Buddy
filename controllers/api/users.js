c23972352ba3988e3e49af6b6488ab6c78604b5e
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-03-01 17:03:43 -0800

    auth

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -1,13 +1,31 @@
+const jwt = require('jsonwebtoken')
+const User = require('../../models/user');
+
 module.exports = {
     create,
 }
 
-function create(req, res) {
-    // Baby step...
-    res.json({
-        user: {
-            name: req.body.name,
-            email: req.body.email
-        }
-    });
+async function create(req, res) {
+    try {
+        // Add the user to the db
+        const user = await User.create(req.body);
+        // token will be a string
+        const token = createJWT(user);
+        // yes we can serialize a string
+        res.json(token);
+    } catch (err) {
+        // probably a duplicate email
+        res.status(400).json(err);
+    }
+}
+
+// helper function
+
+function createJWT(user) {
+    return jwt.sign(
+        // data payload 
+        { user },
+        process.env.SECRET,
+        { expiresIn: '24h' }
+    );
 }

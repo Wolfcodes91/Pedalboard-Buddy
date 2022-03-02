53a7ba339899fe48c30a8b12e26f686d34f7fb4d
Wolfcodes91 <Dave.M.Wolfberg@gmail.com>
2022-03-02 09:31:32 -0800

    user token saved

--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -5,6 +5,27 @@ export async function signUp(userData) {
     // users-api.js module which will ultimately 
     // return the JWT (JSON Web Token)
     const token = await usersAPI.signUp(userData);
-    // baby step by returning whatever is sent back by the server 
+    // persist the token to localStorage
+    localStorage.setItem('token', token);
     return token;
+}
+
+export function getToken() {
+    const token = localStorage.getItem('token');
+    // getItem will return null if no key
+    if (!token) return null 
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    // A JWTs expiration is expressed in seconds, not milliseconds 
+    if (payload.exp < Date.now() / 1000) {
+        // token has expired
+        localStorage.removeItem('token');
+        return null; 
+    }
+    return token;
+}
+
+
+export function getUser() {
+    const token = getToken();
+    return token ? JSON.parse(atob(token.split('.')[1])).user : null;
 }
